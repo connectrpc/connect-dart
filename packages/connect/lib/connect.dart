b377cb1d3981348b12eaeb58d31f122fd21caa6c
Sri Krishna <skrishna@buf.build>
2025-02-10 20:37:26 +0530

    Add package docs
    Signed-off-by: Sri Krishna <skrishna@buf.build>

--- packages/connect/lib/connect.dart
+++ packages/connect/lib/connect.dart
@@ -12,7 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-library connect;
+/// Implementation of the Connect protocol for Dart.
+/// Simple, reliable, interoperable. Protobuf RPC that works
+library;
 
 export 'src/abort.dart';
 export 'src/client.dart';
