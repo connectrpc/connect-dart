86be07ad0a3cf3490ca35b25e7cb1ae0a8f2e82d
Adrien <adrien.vannson@protonmail.com>
2026-07-06 17:50:46 +0200

    Add editions support (#40)
    * Add support for editions
    
    Signed-off-by: Adrien Vannson <avannson@buf.build>

--- packages/connect/test/plugin/plugin_test.dart
+++ packages/connect/test/plugin/plugin_test.dart
@@ -88,6 +88,22 @@ void main() async {
       ),
     );
   });
+  test('generates edition 2024 file', () async {
+    final response = await runPlugin(image, "edition/edition.proto");
+    expect(
+      response,
+      matchGenerated(
+        ['edition/edition.connect.client', 'edition/edition.connect.spec'],
+      ),
+    );
+    expect(
+      response.supportedFeatures &
+          CodeGeneratorResponse_Feature.FEATURE_SUPPORTS_EDITIONS.value,
+      isNot(0),
+    );
+    expect(response.minimumEdition, Edition.EDITION_PROTO2.value);
+    expect(response.maximumEdition, Edition.EDITION_2024.value);
+  });
 }
 
 Future<CodeGeneratorResponse> runPlugin(
